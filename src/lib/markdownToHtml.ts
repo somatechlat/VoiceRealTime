bc9a8884685efe39ae3204ece8e45dd1804bda38
suvanbanerjee <banerjeesuvan@gmail.com>
2025-07-21 23:27:04 +0530

    allow html content

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -7,9 +7,14 @@ import rehypeStringify from "rehype-stringify";
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(remarkGfm)
-    .use(remarkRehype)
+    .use(remarkRehype, {
+      allowDangerousHtml: true,
+      allowedElements: false, // Allow html syntax in markdown
+    })
     .use(rehypeHighlight)
-    .use(rehypeStringify)
+    .use(rehypeStringify, {
+      allowDangerousHtml: true
+    })
     .process(markdown);
   
   return result.toString();
